48e3cd05b9cb89c88fcfdde5b97785fd7d1b9132
madhvi <sehramadhvi@melco-resorts.com>
2017-09-19 16:01:58 +0800

    first commit

---
+++ src/main/java/com/demo/ms/ZuulServiceApplication.java
@@ -0,0 +1,43 @@
+package com.demo.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.demo.ms.filter.ErrorFilter;
+import com.demo.ms.filter.PostFilter;
+import com.demo.ms.filter.PreFilter;
+import com.demo.ms.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}	
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+	
+}
---
+++ src/main/java/com/demo/ms/filter/ErrorFilter.java
@@ -0,0 +1,33 @@
+package com.demo.ms.filter;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class ErrorFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		// TODO Auto-generated method stub
+		System.out.println("Using Route Filter");
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
